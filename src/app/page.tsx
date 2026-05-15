74f72d990e691d6e4dce741e8015ac60c0de1f34
sorodriguezz <s.rodriguezzapata01@gmail.com>
2026-05-15 11:35:27 -0400

    Update basePath in Next.js configuration and correct project image paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { getSortedPostsData } from "@/lib/blog";
 
 export default async function Home() {
   const posts = await getSortedPostsData(true);
-  
+
   return <HomeTemplate initialPosts={posts} />;
 }
-
